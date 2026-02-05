89a674da5b81ea003a5c669e820ed644d7e69540
Ahmed Elhadary <ahmedelhadary.cs@gmail.com>
2026-02-05 15:59:18 +0200

    Fixing images path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/Codera-Official-Website---React/',
+  plugins: [react()]
+});
